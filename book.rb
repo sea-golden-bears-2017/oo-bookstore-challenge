f97f78641e19e6aa42deb84bf35d5a75cfd6ef2c
John Simoni & Amanuel <pair+jsimoni42.yihdego@devbootcamp.com>
2017-09-05 16:06:27 -0700

    passed book tests

--- book.rb
+++ book.rb
@@ -1,9 +1,33 @@
 class Book
-  attr_reader :author, :title
+  attr_reader :author, :title, :quantity, :price, :genre, :isbn,
+   :condition, :publisher, :book_format, :cost, :exploded
 
 
   def initialize(options)
     @author = options[:author]
     @title = options[:title]
+    @quantity = options[:quantity]
+    @price = options[:price]
+    @genre = options[:genre]
+    @isbn = options[:isbn]
+    @condition = options[:condition]
+    @publisher = options[:publisher]
+    @cost = options[:cost]
+    @book_format = options[:book_format]
+    @exploded = false
+  end
+
+  def reduce(num=1)
+    @quantity -= num
+  end
+
+  def increase(num=1)
+    @quantity += num
+  end
+
+  def explode
+    @exploded = true
+    @price = 0
+    reduce
   end
 end
