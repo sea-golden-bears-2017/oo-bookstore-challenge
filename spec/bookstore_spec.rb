c5c8051b843c1a9a4f64090c4f0bf1637442ab08
Eliza Brine & Jamie D <pair+ebrine.seajamied@devbootcamp.com>
2017-09-05 16:27:02 -0700

    Writes rspec tests it statements for BookStore class

--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -1,1 +1,28 @@
+require 'rspec'
 require_relative '../bookstore'
+
+
+describe BookStore do
+context "Bookstore attributes" do
+
+  it "has hours"
+  it "has books"
+  it "has sections"
+  it "has employees"
+  it "can be open"
+  it "has a revenue"
+end
+context "Bookstore behaviors" do
+  it "can hire employees"
+  it "can fire employees"
+  it "can be opened"
+  it "can be closed"
+end
+context "POS behaviors" do
+  it "can make a sale"
+  it "can purchase a book for the bookstore"
+  it "can give a customer a discount"
+  it "can determine whether a customer has a membership"
+  it "can process a return"
+end
+end
