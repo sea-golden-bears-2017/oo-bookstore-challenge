087fb27175d50b22aea456249ce8dcc95eb1fd33
Chris Buell & libby glomb <pair+ChristineBuell.libby-glo@devbootcamp.com>
2017-09-05 16:06:57 -0700

    Added tests for Book class reader class variables

--- book.rb
+++ book.rb
@@ -1,9 +1,19 @@
 class Book
 
-  attr_reader :title
+  attr_reader :title, :author, :isbn, :price, :genre, :publisher, :quantity, :cost, :condition
 
-  def initialize(title)
-    @title = title
+  def initialize(options)
+    @title = options[:title]
+    @author = options[:author]
+    @isbn = options[:isbn]
+    @price = options[:price]
+    @genre = options[:genre]
+    @publisher = options[:publisher]
+    @quantity = options[:quantity]
+    @cost = options[:cost]
+    @condition = options[:condition]
+    p "*" * 50
+    p options
   end
 
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -1,34 +1,51 @@
 require_relative '../book'
 
 describe 'Book class' do
-  let(:book) { Book.new("POODR") }
+  let(:book) { Book.new(
+    :title => "POODR",
+    :author => "Sandi Metz",
+    :isbn => "0-321-72133-0",
+    :price => "44.99",
+    :genre => "Non-Fiction",
+    :publisher => "Addison-Wesley",
+    :quantity => "10",
+    :cost => "22.50",
+    :condition => "New") }
 
   it 'has a title' do
     expect(book.title).to eq "POODR"
   end
 
   it 'has an author' do
+    expect(book.author).to eq "Sandi Metz"
   end
 
   it 'has an isbn' do
+    expect(book.isbn).to eq "0-321-72133-0"
   end
 
   it 'has a price' do
+    expect(book.price).to eq "44.99"
   end
 
   it 'has a genre' do
+    expect(book.genre).to eq "Non-Fiction"
   end
 
   it 'has a publisher' do
+    expect(book.publisher).to eq "Addison-Wesley"
   end
 
   it 'has a quantity' do
+    expect(book.quantity).to eq "10"
   end
 
   it 'has a cost' do
+    expect(book.cost).to eq "22.50"
   end
 
   it 'has a condition' do
+    expect(book.condition).to eq "New"
   end
 
 end
