2caba8bd61fb26ab7df559766152757189b1e0df
Gordon Baker & Piper Denney <pair+gorb88.piperdenney@devbootcamp.com>
2017-09-05 16:14:38 -0700

    Implemented getters for book data. Wrote and passed corresponding test.

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_reader :id, :title, :author, :genre, :price, :count, :isbn
+
+  def initialize(book_hash)
+    @id = book_hash[:id]
+    @title = book_hash[:title]
+    @author = book_hash[:author]
+    @genre = book_hash[:genre]
+    @price = book_hash[:price]
+    @count = book_hash[:count]
+    @isbn = book_hash[:isbn]
+
+
+  end
+end
