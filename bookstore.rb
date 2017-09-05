65e7ab369618e6767aac0940d9198bd01311fc07
John Simoni & Amanuel <pair+jsimoni42.yihdego@devbootcamp.com>
2017-09-05 16:40:28 -0700

    Passed bookstore tests

--- bookstore.rb
+++ bookstore.rb
@@ -1,8 +1,20 @@
 class BookStore
-  attr_reader :hours, :sections
+  attr_reader :hours, :sections, :books_inventory, :employees, :open
 
   def initialize(options)
     @hours = options[:hours]
     @sections = options[:sections]
+    @books_inventory = {}
+    @employees = {}
+    @open = false
+    @revenue = 0
+  end
+
+  def open?
+    @open
+  end
+
+  def book_store_open
+    @open = true
   end
 end
--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -14,4 +14,21 @@ describe BookStore do
   it 'has two large sections of fiction and non-fiction' do
     expect(book_store.sections.keys).to eq(["Fiction", "Non-Fiction"])
   end
+
+  it 'has the capacity to check inventory' do
+    expect(book_store.books_inventory).to eq({})
+  end
+
+  it 'has the capacity to add employees' do
+    expect(book_store.employees).to eq({})
+  end
+
+  it 'is closed when initialized' do
+    expect(book_store.open?).to eq(false)
+  end
+
+  it "can open at 10 o'clock" do
+    book_store.book_store_open
+    expect(book_store.open?).to eq(true)
+  end
 end
