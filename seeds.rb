2a8e62d0a02a55fb465bb218aefb938fb5c2a3a4
Jordan Kamin <jordanakamin@gmail.com>
2017-09-05 10:24:31 -0700

    adds .gitignore

--- seeds.rb
+++ seeds.rb
@@ -3,4 +3,9 @@ require 'csv'
 
 CSV.open("invoice.csv", "wb") do |csv|
   csv << ['id', 'title', 'author', 'genre', 'price', 'count', 'isbn']
+  i = 1
+  100.times do
+    csv << [i, Faker::Book.title, Faker::Name.name, Faker::Book.genre, Faker::Commerce.price, (rand * 10).floor ]
+    i += 1
+  end
 end
