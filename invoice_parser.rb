05617e4021452cd3707f3f732ecc685833527e5d
Jordan Kamin <jordanakamin@gmail.com>
2017-09-05 10:36:43 -0700

    builds invoice_parser class

---
+++ invoice_parser.rb
@@ -0,0 +1,26 @@
+require 'faker'
+require 'csv'
+
+class InvoiceParser
+  def self.write
+    CSV.open("invoice.csv", "wb") do |csv|
+      csv << ['id', 'title', 'author', 'genre', 'price', 'count', 'isbn']
+      i = 1
+      100.times do
+        csv << [i, Faker::Book.title, Faker::Name.name, Faker::Book.genre, Faker::Commerce.price, Faker::Number.between(1, 30), self.isbn ]
+        i += 1
+      end
+    end
+  end
+
+  private
+
+  def self.isbn
+    book_no = "0-"
+    book_no << Faker::Number.number(3)
+    book_no << '-'
+    book_no << Faker::Number.number(6)
+    book_no << '-'
+    book_no << Faker::Number.number(1)
+  end
+end
--- seeds.rb
+++ seeds.rb
@@ -1,11 +1,3 @@
-require 'faker'
-require 'csv'
+require_relative 'invoice_parser'
 
-CSV.open("invoice.csv", "wb") do |csv|
-  csv << ['id', 'title', 'author', 'genre', 'price', 'count', 'isbn']
-  i = 1
-  100.times do
-    csv << [i, Faker::Book.title, Faker::Name.name, Faker::Book.genre, Faker::Commerce.price, (rand * 10).floor ]
-    i += 1
-  end
-end
+InvoiceParser.write
