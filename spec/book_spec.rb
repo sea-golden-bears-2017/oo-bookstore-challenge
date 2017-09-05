f5b62ba346d2cdd092f87ad0595083f582b2797a
Gordon Baker & Piper Denney <pair+gorb88.piperdenney@devbootcamp.com>
2017-09-05 15:42:54 -0700

    Change ruby version in gem and ruby. Start tests for bookspec.

---
+++ spec/book_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require_relative '../book.rb'
+
+
+describe 'book' do
+  let(:book) { Book.new }
+
+  it 'has an id' do
+
+  end
+  it 'has a title' do
+
+  end
+  it 'has an author' do
+
+  end
+  it 'has a genre' do
+
+  end
+  it 'has a price' do
+
+  end
+  it 'has a count' do
+
+  end
+
+  it 'has an isbn' do
+
+  end
+
+end
