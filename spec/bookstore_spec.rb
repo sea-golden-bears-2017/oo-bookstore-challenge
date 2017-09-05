9992a61a81b58cef6f8e7fd0af604ca1bb0a0e2f
Anna Jeszeck & Devin Miller <pair+ajeszeck.devzmiller@devbootcamp.com>
2017-09-05 16:37:24 -0700

    Add bookstore class and tests

--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -1,27 +1,31 @@
 require_relative '../bookstore'
+require_relative '../book'
 
 describe BookStore do
-  it "has some books" do
 
+  let(:bookstore) { BookStore.new }
+
+  it "purchases books and adds them to the inventory" do
+    bookstore.purchase('invoice.csv')
+    expect(bookstore.books.length).to_not be 0
   end
 
-  it "has some employees" do
+  # it "has some employees" do
+  #   expect(bookstore.employees.length).to_not be 0
+  # end
 
-  end
-
-  it "it had some shelves" do
-
-  end
+  # it "it had some shelves" do
+  #
+  # end
 
   it "it is open after the open method is called" do
-
+    expect(bookstore.toggle_open_status).to be true
   end
 
   it "it is closed after the close method is called" do
-
+    bookstore.toggle_open_status
+    expect(bookstore.toggle_open_status).to be false
   end
 
-  it "purchases books and adds them to the inventory" do
 
-  end
 end
