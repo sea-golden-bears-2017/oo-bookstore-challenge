c8d473f0219809894c9575c47c5c956ae47f279b
Anna Jeszeck & Devin Miller <pair+ajeszeck.devzmiller@devbootcamp.com>
2017-09-05 16:03:44 -0700

    Write pending bookstore tests

--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -1,1 +1,27 @@
 require_relative '../bookstore'
+
+describe BookStore do
+  it "has some books" do
+
+  end
+
+  it "has some employees" do
+
+  end
+
+  it "it had some shelves" do
+
+  end
+
+  it "it is open after the open method is called" do
+
+  end
+
+  it "it is closed after the close method is called" do
+
+  end
+
+  it "purchases books and adds them to the inventory" do
+
+  end
+end
