56a02f48a193ea84c3fdb5cc6930fdc950b87259
Ashishjayandar & Marlo Ito <pair+ashishjayandar.marloito@devbootcamp.com>
2017-09-05 19:05:31 -0700

    BookStore class done -- all tests pass

--- bookstore.rb
+++ bookstore.rb
@@ -1,2 +1,27 @@
 class BookStore
+  attr_reader :hours, :books, :sections, :employees, :revenue
+
+  def initialize
+    @hours = Range.new(9,18)
+    @books = []
+    # @sections = []
+    @employees = []
+    @revenue = 0
+  end
+
+  def open?
+    current_time = Time.now.strftime("%H.%M").to_f
+    return true if @hours.include?(current_time)
+    false
+  end
+
+  def hire(employee)
+    @employees << employee
+  end
+
+  def fire(employee)
+    # when declaring the employee class make sure to access the employees using id
+    # remember that employee information will be in held of a hash hash[:symbol]
+    @employees.delete(employee)
+  end
 end
--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -1,1 +1,41 @@
 require_relative '../bookstore'
+
+describe BookStore do
+  let(:store) { BookStore.new }
+  let(:employee) { "will be an instance of employee class" }
+
+  it 'has store hours' do
+    expect(store.hours.class).to eq Range
+  end
+
+  it 'has books' do
+    expect(store.books.class).to eq Array
+  end
+
+  # it 'has sections' do
+  #   expect(store.sections.class).to eq Array
+  # end
+
+  it 'has employees' do
+    expect(store.employees.class).to eq Array
+  end
+
+  it 'has revenue' do
+    expect(store.revenue.class).to eq Fixnum
+  end
+
+  it 'has returns true if open?'do
+    expect(store.open?).to be false
+  end
+
+  it 'can hire employees' do
+    store.hire(employee)
+    expect(store.employees.length).to eq 1
+  end
+
+  it 'can fire employees' do
+    store.hire(employee)
+    store.fire(employee)
+    expect(store.employees).to eq []
+  end
+end
