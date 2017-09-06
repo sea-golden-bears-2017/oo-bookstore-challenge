21c76927fbcb02e31cdb311f95bfb969d807fd5c
Eliza Brine & Jamie D <pair+ebrine.seajamied@devbootcamp.com>
2017-09-05 18:05:46 -0700

    Adds open and close store methods

--- bookstore.rb
+++ bookstore.rb
@@ -1,11 +1,6 @@
-
-
-
-
-
 class BookStore
-
-  attr_reader :hours, :books, :sections, :employees, :is_open, :revenue
+  attr_accessor :is_open
+  attr_reader :hours, :books, :sections, :employees, :revenue
 
   def initialize(name)
     @name = name
@@ -17,4 +12,22 @@ class BookStore
     @revenue = 0.0
   end
 
+  def hire_employee(name)
+    # employees << Employee.new(name)
+    p "Connect me to Employee class"
+  end
+
+  def fire(id)
+    # find the employee with the id
+    # set terminated to true
+    employees
+  end
+
+  def open_store
+    @is_open = true
+  end
+
+  def close_store
+    @is_open = false
+  end
 end
