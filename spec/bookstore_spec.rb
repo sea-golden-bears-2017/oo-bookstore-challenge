179c3b693a197c42208b48cf3b6770083a936bf7
John Simoni & Amanuel <pair+jsimoni42.yihdego@devbootcamp.com>
2017-09-05 17:06:22 -0700

    added beggings of employee class and tested it in bookstore

--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -2,10 +2,19 @@ require 'rspec'
 require_relative '../book'
 require_relative '../bookstore'
 
+class Employee
+  attr_reader :id, :name
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+end
+
 describe BookStore do
+
   let (:book_store) { BookStore.new({hours: "10 AM - 10 PM",
                               sections: {"Fiction" => [],
-                                        "Non-Fiction" => []}}) }
+                                        "Non-Fiction" => []}})}
 
   it 'lists hours from 10 am to 10pm' do
     expect(book_store.hours).to eq("10 AM - 10 PM")
@@ -30,5 +39,22 @@ describe BookStore do
   it "can open at 10 o'clock" do
     book_store.book_store_open
     expect(book_store.open?).to eq(true)
+  end
+
+  it 'revenue starts at 0' do
+    expect(book_store.revenue).to eq(0)
+  end
+
+  it 'can hire employees' do
+    employee = Employee.new(1,"Amanuel")
+    book_store.hire(employee)
+    expect(book_store.employees.keys).to eq([employee.id])
+  end
+
+  it 'can fire employees' do
+    employee = Employee.new(1,"Amanuel")
+    book_store.hire(employee)
+    book_store.fire(employee)
+    expect(book_store.employees).to eq({})
   end
 end
