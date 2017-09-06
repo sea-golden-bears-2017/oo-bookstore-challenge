ed847cd985336fa7a18e5d4074d58cd95cbf9634
Ashishjayandar & Marlo Ito <pair+ashishjayandar.marloito@devbootcamp.com>
2017-09-05 20:43:27 -0700

    adding basic point-of-sale files -- Module"

---
+++ point_of_sale.rb
@@ -0,0 +1,2 @@
+module PointOfSale
+end
---
+++ spec/point_of_sale_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../point_of_sale'
+
+describe PointOfSale do
+
+  it 'sell books'
+  it 'purchase inventory -- books'
+  it 'apply discounts'
+  it 'check for memebership'
+  it 'record a transaction'
+end
