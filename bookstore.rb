bb88aa3544be831fb8e754767db3f80fd85dea14
Gordon Baker & Piper Denney <pair+gorb88.piperdenney@devbootcamp.com>
2017-09-05 17:18:24 -0700

    Write tests and methods for BookStore

--- bookstore.rb
+++ bookstore.rb
@@ -1,2 +1,56 @@
+require_relative 'book'
+
 class BookStore
+  attr_reader :books, :employees, :revenue
+
+  def initialize
+    @books = []
+    @employees = []
+    @revenue = 1.0
+  end
+
+  def add_book(book)
+    @books << book
+  end
+
+  def add_employee(employee)
+    @employees << employee
+  end
+
+
 end
+
+
+
+# bookstore
+#
+# books
+# employees
+# revenue
+# transactions
+#
+#
+# POS (module)
+#
+# sell
+# purchase
+# return
+# discount
+#
+#
+# transaction
+#
+# id
+# items
+# revenue
+# employee_id
+# cash
+# change
+#
+#
+# employee
+#
+# name
+# position
+# id
+# auth level
--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -1,1 +1,25 @@
 require_relative '../bookstore'
+require_relative '../employee'
+
+describe BookStore do
+  let(:book_hash) {{:id => "1",:title => "After Many a Summer Dies the Swan", :author => "Mustafa Mayer MD", :genre => "Fairy tale", :price => "83.42", :count => "17", :isbn => "0-194-607033-0"}}
+  let(:book) { Book.new(book_hash) }
+  let(:employee) { Employee.new }
+
+  let(:bookstore) { BookStore.new }
+
+  it 'has books' do
+    bookstore.add_book(book)
+    expect(bookstore.books.empty?).to eq(false)
+  end
+
+  it 'has employees' do
+    bookstore.add_employee(employee)
+    expect(bookstore.employees.empty?).to eq(false)
+  end
+
+  it 'has revenue' do
+    expect(bookstore.revenue).to eq(1.0)
+  end
+  it 'has transactions'
+end
