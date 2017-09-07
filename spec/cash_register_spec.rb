8ac2a2613686e7824223d76c092de67e1f04a5ce
Anna Jeszeck & Taylor Svendsen <pair+ajeszeck.galactusby@devbootcamp.com>
2017-09-07 16:24:33 -0700

    Add tests and beginning of methods for cash register class

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,8 +1,11 @@
 require 'rspec'
 require_relative '../cash_register'
+require_relative '../book'
 
 describe CashRegister do
   let(:cash_register) { CashRegister.new }
+  let(:book) { Book.new({id: 7, title: "In a Glass Darkly", author: "Miss Marisa Balistreri", genre: "Crime/Detective", price: 29.63, count: 24, isbn: "0-599-648299-6"}) }
+
   describe 'attributes' do
     it 'has a till' do
       expect(cash_register.till).to eq(0)
@@ -14,13 +17,17 @@ describe CashRegister do
   end
 
   describe 'methods' do
-    xit '#sell' do
+    it '#sell' do
+      cash_register.sell(book.id)
+      expect(cash_register.transactions).to eq [transaction_object]
     end
 
     xit '#buy' do
+      # Something like if we have enough money in the till we will buy the book
     end
 
     xit '#transaction' do
+      cash_register.transaction()
     end
 
     xit '#return' do
