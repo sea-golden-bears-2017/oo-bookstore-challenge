53ecf5618f75bd565d59af3dc3dd22ec4858891e
Eliza Brine & Piper Denney <pair+ebrine.piperdenney@devbootcamp.com>
2017-09-07 16:10:47 -0700

    converted the values of price, quantity and cost in initialize to numbers

--- product.rb
+++ product.rb
@@ -3,9 +3,9 @@ class Product
   attr_reader :price, :quantity, :cost, :shelf, :condition
 
   def initialize(product_hash)
-    @price = product_hash[:price]
-    @quantity = product_hash[:quantity]
-    @cost = product_hash[:cost]
+    @price = product_hash[:price].to_f
+    @quantity = product_hash[:quantity].to_i
+    @cost = product_hash[:cost].to_i
     @shelf = product_hash[:shelf]
     @condition = product_hash[:condition]
   end
