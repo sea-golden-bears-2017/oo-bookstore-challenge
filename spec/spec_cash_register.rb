fbb4563490a8c4e7b80de1414eadb130679eed3d
Ashishjayandar & Jamie D <pair+ashishjayandar.seajamied@devbootcamp.com>
2017-09-07 16:45:17 -0700

    Add tests in cash register spec sheet

---
+++ spec/spec_cash_register.rb
@@ -0,0 +1,16 @@
+require_relative '../cash_register.rb'
+
+describe CashRegister do
+  context 'Attribute tests' do
+    let(:register) { CashRegister.new() }
+
+    it 'has transactions' do
+      expect(register.transactions).to eq []
+    end
+
+    it 'has a till' do
+      expect(register.till).to eq ({twenty: 5, ten: 10, five: 20, one: 100})
+    end
+
+  end
+end
