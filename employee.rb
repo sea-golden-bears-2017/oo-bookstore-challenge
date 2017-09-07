91e336188eabd6701b191ef60afe2d9602ca503a
Chris Buell & Leonard Dain <pair+ChristineBuell.ldain281@devbootcamp.com>
2017-09-07 10:54:21 -0700

    initial tests for employee class written

--- employee.rb
+++ employee.rb
@@ -1,4 +1,7 @@
+require 'date'
 class Employee
+
+  attr_reader :name, :id, :wage, :position, :ssn, :authorization, :hire_date, :termination_date
 
   def initialize(args)
     @name = args[:name]
@@ -7,8 +10,8 @@ class Employee
     @position = args[:position]
     @ssn = args[:ssn]
     @authorization = args[:authorization]
-    @hire = args[:hire]
-    @termination = args[:termination]
-
+    @hire_date = args[:hire_date]
+    @termination_date = args[:termination_date]
   end
+
 end
