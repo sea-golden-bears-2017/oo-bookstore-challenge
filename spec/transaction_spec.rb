f27eb8252c929aeb5d56635ae9b2945b95f2e933
Anna Jeszeck & Taylor Svendsen <pair+ajeszeck.galactusby@devbootcamp.com>
2017-09-07 16:24:07 -0700

    Create new files for transaction class and tests

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../cash_register'
+
+describe Transaction do
+  describe 'attributes' do
+    it 'has an id'
+    it 'includes items'
+    it 'has revenue'
+    it 'has employee id'
+    it 'records cash given'
+    it 'returns change'
+  end
+end
