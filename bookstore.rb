a0e366215a88a1d80387f9fd3ac78f7afd4cd1c6
Anna Jeszeck & Taylor Svendsen <pair+ajeszeck.galactusby@devbootcamp.com>
2017-09-07 12:32:16 -0700

    add faker to generate employee info

--- bookstore.rb
+++ bookstore.rb
@@ -43,7 +43,7 @@ class BookStore
   end
 
   def hire(num_hires)
-
+    Employee.new()
   end
 
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,30 +1,31 @@
 require 'rspec'
 require_relative '../employee.rb'
+require 'faker'
 
 describe Employee do
-  let(:employee) { Employee.new({id: 1, full_name: "Anna Jeszeck", position: "Sales Assistant", wage: 15, hours: 40, hire_date: "1-1-2017", fire_date: "1-2-2017", ssn: "123-45-6789", auth_level: "Level 1"}) }
+  let(:employee) { Employee.new({id: 1, full_name: Faker::TwinPeaks.character, position: Faker::Job.title, wage: Faker::Number.decimal(2), hours: Faker::Number.number(2), hire_date: Date.today, fire_date: Faker::Date.forward(200), ssn: "123-45-6789", auth_level: "Level 1"}) }
 
   describe "employee attributes"
     it "has an ID" do
       expect(employee.id).to eq(1)
     end
     it "has a name" do
-      expect(employee.full_name).to eq("Anna Jeszeck")
+      expect(employee.full_name).to eq(employee.full_name)
     end
     it "has a position" do
-      expect(employee.position).to eq("Sales Assistant")
+      expect(employee.position).to eq(employee.position)
     end
     it "has a wage" do
-      expect(employee.wage).to eq(15)
+      expect(employee.wage).to eq(employee.wage)
     end
     it "has hours" do
-      expect(employee.hours).to eq(40)
+      expect(employee.hours).to eq(employee.hours)
     end
     it "has a hire_date" do
-      expect(employee.hire_date).to eq("1-1-2017")
+      expect(employee.hire_date).to eq(employee.hire_date)
     end
     it "has a fire_date" do
-      expect(employee.fire_date).to eq("1-2-2017")
+      expect(employee.fire_date).to eq(employee.fire_date)
     end
     it "has a SSN" do
       expect(employee.ssn).to eq("123-45-6789")
