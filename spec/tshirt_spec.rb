73f754bb4785025d87b3f059b5fb51871949cf8a
Mandil & Marlo Ito <pair+mandil.marloito@devbootcamp.com>
2017-09-07 17:30:41 -0700

    write #reject? for Tshirt -- tests pass

--- spec/tshirt_spec.rb
+++ spec/tshirt_spec.rb
@@ -3,6 +3,7 @@ require_relative '../tshirt'
 
 describe Tshirt do
   let(:shirt) { Tshirt.new({id: '00798', price: 25, count: 30, size: 'medium'}) }
+  let(:bad_shirt) { Tshirt.new({id: '00798', price: 25, count: 30, size: 'medium'}, condition = 'bad') }
   context 'attributes' do
     it 'has an id' do
       expect(shirt.id).to eq '00798'
@@ -17,6 +18,23 @@ describe Tshirt do
 
     it 'it has a size' do
       expect(shirt.size).to eq 'medium'
+    end
+
+    describe '#reject?' do
+      it 'returns true if condition is bad' do
+        expect(bad_shirt.reject?(bad_shirt.condition)).to be true
+      end
+      it 'returns true if condition is new' do
+        expect(shirt.reject?(shirt.condition)).to be true
+      end
+      it 'returns false if condition is good' do
+        bad_shirt.condition = 'good'
+        expect(bad_shirt.reject?(bad_shirt.condition)).to be false
+      end
+    end
+
+    describe "#appraise" do
+      it 'returns the discouted price on the product'
     end
   end
 end
--- tshirt.rb
+++ tshirt.rb
@@ -2,9 +2,17 @@ require_relative 'product'
 
 class Tshirt < Product
   attr_reader :id, :price, :count, :size
+  attr_accessor :condition
 
-  def initialize(args)
+  def initialize(args, condition = 'new')
     super({id: args[:id], count: args[:count], price: args[:price]})
     @size = args[:size]
+    @condition = condition
   end
+
+  def reject?(condition)
+    return true if @condition == 'bad' || @condition == 'new'
+    false
+  end
+
 end
