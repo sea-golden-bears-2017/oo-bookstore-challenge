49753f4d46fde9c7cb318928390597db2e79e50f
Mandil & Marlo Ito <pair+mandil.marloito@devbootcamp.com>
2017-09-07 18:41:48 -0700

    write #appraise -- tests pass

--- spec/tshirt_spec.rb
+++ spec/tshirt_spec.rb
@@ -3,7 +3,8 @@ require_relative '../tshirt'
 
 describe Tshirt do
   let(:shirt) { Tshirt.new({id: '00798', price: 25, count: 30, size: 'medium'}) }
-  let(:bad_shirt) { Tshirt.new({id: '00798', price: 25, count: 30, size: 'medium'}, condition = 'bad') }
+  let(:other_shirt) { Tshirt.new({id: '00798', price: 25, count: 30, size: 'medium'}, condition = 'bad') }
+
   context 'attributes' do
     it 'has an id' do
       expect(shirt.id).to eq '00798'
@@ -21,20 +22,59 @@ describe Tshirt do
     end
 
     describe '#reject?' do
-      it 'returns true if condition is bad' do
-        expect(bad_shirt.reject?(bad_shirt.condition)).to be true
-      end
-      it 'returns true if condition is new' do
-        expect(shirt.reject?(shirt.condition)).to be true
+      context "returns true" do
+        it 'if condition is bad' do
+          expect(other_shirt.reject?(other_shirt.condition)).to be true
+        end
+        it 'if condition is new' do
+          expect(shirt.reject?(shirt.condition)).to be true
+        end
       end
       it 'returns false if condition is good' do
-        bad_shirt.condition = 'good'
-        expect(bad_shirt.reject?(bad_shirt.condition)).to be false
+        other_shirt.condition = 'good'
+        expect(other_shirt.reject?(other_shirt.condition)).to be false
       end
     end
 
     describe "#appraise" do
-      it 'returns the discouted price on the product'
+      context "raises an error" do
+        let(:condition){other_shirt.condition}
+        let(:price){other_shirt.price}
+        let(:condition_2){shirt.condition}
+        let(:price_2){shirt.price}
+
+        it "when condition is 'bad'" do
+          expect{other_shirt.appraise(condition, price)}.to raise_error(Tshirt::InvalidCondition)
+        end
+        it "when condition is 'new'" do
+          expect{shirt.appraise(condition_2, price_2)}.to raise_error(Tshirt::InvalidCondition)
+        end
+      end
+      context "returns the discounted price based on the condition of the product" do
+        let(:condition){other_shirt.condition}
+        let(:price){other_shirt.price}
+
+        it "returns 17.5 if condition is 'good'" do
+          other_shirt.condition = 'good'
+          expect(other_shirt.appraise(condition, price)).to eq 18.75
+        end
+        it "returns 12.5 if condition is 'okay'"do
+          other_shirt.condition = 'okay'
+          expect(other_shirt.appraise(condition, price)).to eq 12.5
+        end
+
+      end
+    end
+
+    describe '#find_discount' do
+      it 'returns 0.25 if condition is good' do
+        other_shirt.condition = 'good'
+        expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.25
+      end
+      it 'returns 0.5 if condition is okay' do
+        other_shirt.condition = 'okay'
+        expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.5
+      end
     end
   end
 end
--- tshirt.rb
+++ tshirt.rb
@@ -10,9 +10,24 @@ class Tshirt < Product
     @condition = condition
   end
 
+  class InvalidCondition < StandardError
+  end
+
   def reject?(condition)
     return true if @condition == 'bad' || @condition == 'new'
     false
+  end
+
+  def find_discount(condition)
+    return 0.25 if condition == 'good'
+    return 0.5 if condition == 'okay'
+  end
+
+  def appraise(condition, price)
+    raise InvalidCondition if reject?(condition)
+
+    discount = find_discount(condition)
+    price - (price * discount)
   end
 
 end
