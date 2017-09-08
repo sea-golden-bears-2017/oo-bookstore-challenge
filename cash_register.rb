8b0b94f88589f4893fa93bcacae12eea8353df55
Eliza Brine & Piper Denney <pair+ebrine.piperdenney@devbootcamp.com>
2017-09-07 18:26:55 -0700

    Creates transactions and till variables and sell and creat_transaction. still working on methods

--- cash_register.rb
+++ cash_register.rb
@@ -1,3 +1,37 @@
-class CashRegister
+require_relative 'transaction'
 
+
+class CashRegister
+  attr_reader :transactions, :till
+
+  def initialize
+    @transactions = []
+    @till = {hundred: 0, fifty: 0, twenty: 0, ten: 0, five: 0, one: 0}
+  end
+
+  def sell(product_array, employee)
+    # I: accept a list of products to sell
+    # I: employee (get the id and auth)
+    # I: customer (in order to pay/ get "cash")
+    # accept money payment and calculate change
+    # remove items from inventory
+    # create a transaction - (maybe sends to transaction method)
+    book = product_array[0]
+    book.quantity = book.quantity - 1
+    # calculate total price
+    # ask customer for cash
+      # cash = customer cash
+    # calculate change
+    create_transaction()
+  end
+
+  def create_transaction(product_array, employee, cash, )
+    # generates new id
+      # id is index of transaction in transactions array
+    # logs items sold
+    # logs charge
+    # logs employee id
+    # logs cash accepted
+    # logs change
+  end
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,9 +1,33 @@
 require 'rspec'
 require_relative '../transaction'
+require_relative '../cash_register'
+require_relative "../book"
+require_relative '../employee'
 
 describe CashRegister do
-  xit 'has transactions' do
-    bookstore.add_transaction(transaction)
-    expect(bookstore.transactions.empty?).to eq(false)
+  let(:register) { CashRegister.new }
+
+  context "attributes" do
+    it 'has transactions' do
+      expect(register.transactions).to eq []
+    end
+
+    it 'has a till' do
+      expect(register.till).to be_an_instance_of Hash
+    end
   end
+
+  describe "#sell" do
+    let(:book_hash) {{:id => "1",
+      :title => "After Many a Summer Dies the Swan",
+      :author => "Mustafa Mayer MD", :genre => "Fairy tale",
+      :price => "83.42", :quantity=> "17", :isbn => "0-194-607033-0"}}
+    let(:book) { Book.new(book_hash) }
+    let(:employee) { Employee.new({id: 1, name: "Frank"}) }
+
+    it "changes quantity of product" do
+      expect{ register.sell([book], employee) }.to change { book.quantity }.from(17).to(16)
+    end
+  end
+
 end
