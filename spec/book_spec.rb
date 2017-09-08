22aa72a5be49d34543a638db0838293577749a84
Mandil & Marlo Ito <pair+mandil.marloito@devbootcamp.com>
2017-09-07 19:40:17 -0700

    incorporate Appraisable module into Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../book'
+require_relative '../appraise'
 
 describe 'Book' do
   let(:book) {Book.new({
@@ -7,7 +8,7 @@ describe 'Book' do
     title: "The Yellow Meads of Asphodel",
     author: 'Miss Gerardo Harber',
     genre: 'Mythopoeia',
-    price: 10.37,
+    price: 10,
     count: 24,
     isbn: '0-278-814673-6',
     })}
@@ -29,7 +30,7 @@ describe 'Book' do
   end
 
   it 'has a price' do
-    expect(book.price).to eq(10.37)
+    expect(book.price).to eq(10)
   end
 
   it 'has quantity' do
@@ -38,6 +39,35 @@ describe 'Book' do
 
   it 'has an isbn' do
     expect(book.isbn).to eq("0-278-814673-6")
+  end
+
+  describe "#set_discount_price" do
+    context 'raises an error if' do
+      it 'condition is new' do
+        expect{ book.set_discount_price(book.condition, book.price) }.to raise_error(Appraisable::InvalidCondition)
+      end
+
+      it 'condition is bad' do
+        book.condition = 'bad'
+
+        expect{ book.set_discount_price(book.condition, book.price) }.to raise_error(Appraisable::InvalidCondition)
+      end
+    end
+    context 'it updates the price attribute' do
+      it 'to 5 if condition is okay' do
+        book.condition = 'okay'
+        book.set_discount_price(book.condition, book.price)
+
+        expect(book.price).to eq 5
+      end
+
+      it 'to 7.5 if condition is good' do
+        book.condition = 'good'
+        book.set_discount_price(book.condition, book.price)
+
+        expect(book.price).to eq 7.5
+      end
+    end
   end
 
 end
--- spec/tshirt_spec.rb
+++ spec/tshirt_spec.rb
@@ -14,6 +14,7 @@ describe Tshirt do
     it 'has a price' do
       expect(shirt.price).to eq 25
     end
+
     it 'has a count' do
       expect(shirt.count).to eq 30
     end
@@ -21,70 +22,74 @@ describe Tshirt do
     it 'it has a size' do
       expect(shirt.size).to eq 'medium'
     end
+  end
 
-    describe '#reject?' do
-      context "returns true" do
-        it 'if condition is bad' do
-          expect(other_shirt.reject?(other_shirt.condition)).to be true
-        end
-        it 'if condition is new' do
+  describe '#reject?' do
+    context "returns true" do
+      it 'if condition is bad' do
+        expect(other_shirt.reject?(other_shirt.condition)).to be true
+      end
+      it 'if condition is new' do
           expect(shirt.reject?(shirt.condition)).to be true
-        end
       end
-      it 'returns false if condition is good' do
+    end
+
+    it 'returns false if condition is good' do
+      other_shirt.condition = 'good'
+      expect(other_shirt.reject?(other_shirt.condition)).to be false
+    end
+
+  describe "#appraise" do
+    context "raises an error" do
+      let(:condition){other_shirt.condition}
+      let(:price){other_shirt.price}
+
+      let(:condition_2){shirt.condition}
+      let(:price_2){shirt.price}
+
+      it "when condition is 'bad'" do
+        expect{other_shirt.appraise(condition, price)}.toraise_error(Tshirt::InvalidCondition)
+      end
+
+      it "when condition is 'new'" do
+        expect{shirt.appraise(condition_2, price_2)}.toraise_error(Tshirt::InvalidCondition)
+      end
+    end
+
+    context "returns the discounted price based on the condition of theproduct" do
+      let(:condition){other_shirt.condition}
+      let(:price){other_shirt.price}
+      it "returns 17.5 if condition is 'good'" do
         other_shirt.condition = 'good'
-        expect(other_shirt.reject?(other_shirt.condition)).to be false
+        expect(other_shirt.appraise(condition, price)).to eq 18.75
       end
-    end
 
-    describe "#appraise" do
-      context "raises an error" do
-        let(:condition){other_shirt.condition}
-        let(:price){other_shirt.price}
-        let(:condition_2){shirt.condition}
-        let(:price_2){shirt.price}
-
-        it "when condition is 'bad'" do
-          expect{other_shirt.appraise(condition, price)}.to raise_error(Tshirt::InvalidCondition)
-        end
-        it "when condition is 'new'" do
-          expect{shirt.appraise(condition_2, price_2)}.to raise_error(Tshirt::InvalidCondition)
-        end
-      end
-      context "returns the discounted price based on the condition of the product" do
-        let(:condition){other_shirt.condition}
-        let(:price){other_shirt.price}
-
-        it "returns 17.5 if condition is 'good'" do
-          other_shirt.condition = 'good'
-          expect(other_shirt.appraise(condition, price)).to eq 18.75
-        end
-        it "returns 12.5 if condition is 'okay'"do
-          other_shirt.condition = 'okay'
-          expect(other_shirt.appraise(condition, price)).to eq 12.5
-        end
-
-      end
-    end
-
-    describe '#find_discount' do
-      it 'returns 0.25 if condition is good' do
-        other_shirt.condition = 'good'
-        expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.25
-      end
-      it 'returns 0.5 if condition is okay' do
+      it "returns 12.5 if condition is 'okay'"do
         other_shirt.condition = 'okay'
-        expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.5
+        expect(other_shirt.appraise(condition, price)).to eq 12.5
       end
-    end
-
-    describe "#set_discount_price" do
-      it 'update the price attribute' do
-        other_shirt.condition = 'okay'
-        other_shirt.set_discount_price(other_shirt.condition, other_shirt.price)
-        expect(other_shirt.price).to eq 12.5
-      end
-
     end
   end
+
+  describe '#find_discount' do
+    it 'returns 0.25 if condition is good' do
+      other_shirt.condition = 'good'
+      expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.25
+    end
+
+    it 'returns 0.5 if condition is okay' do
+      other_shirt.condition = 'okay'
+      expect(other_shirt.find_discount(other_shirt.condition)).to eq 0.5
+    end
+  end
+
+  describe "#set_discount_price" do
+    it 'update the price attribute' do
+      other_shirt.condition = 'okay'
+      other_shirt.set_discount_price(other_shirt.condition, other_shirt.price)
+
+      expect(other_shirt.price).to eq 12.5
+    end
+  end
+end
 end
